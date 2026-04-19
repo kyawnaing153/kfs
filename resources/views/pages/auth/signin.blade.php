54b11e9695f42b83471fdc88bbbaaa520c2251b1
Ajeet Kumar <ajeetkumr567@gmail.com>
2026-04-19 22:45:18 +0630

    update sign

--- resources/views/pages/auth/signin.blade.php
+++ resources/views/pages/auth/signin.blade.php
@@ -34,7 +34,7 @@
                                         <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
                                             Phone<span class="text-error-500">*</span>
                                         </label>
-                                        <input type="text" id="phone" name="phone" placeholder="09********99" value="09977348597"
+                                        <input type="text" id="phone" name="phone" placeholder="09********99"
                                             class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-gray-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" />
                                     </div>
                                     <!-- Password -->
@@ -44,7 +44,7 @@
                                         </label>
                                         <div x-data="{ showPassword: false }" class="relative">
                                             <input :type="showPassword ? 'text' : 'password'"
-                                                name="password" value="12345678"
+                                                name="password"
                                                 placeholder="Enter your password"
                                                 class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-gray-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pr-11 pl-4 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30" />
                                             <span @click="showPassword = !showPassword"
